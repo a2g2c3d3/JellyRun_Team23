44f8000ebe867aa09eae764fb341440db20d44e1
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-07-29 20:42:16 +0900

    Mesh Pro 파일을 깠기 때문에 파일이 많습니다!
    Feat: 메인 씬으로 가는 버튼과 유저 애니메이션 추가(LobyScene)

--- Assets/Scripts/GameFlowManager/LobyManager.cs
+++ Assets/Scripts/GameFlowManager/LobyManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LobyManager : MonoBehaviour
 {
+    public Button button;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        button.onClick.AddListener(GameStartButton);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
+
+     void GameStartButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
 }
