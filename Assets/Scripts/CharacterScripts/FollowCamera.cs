2b9a97dde182dfb2bc9935eb7e4b3e025477b337
Seungmin7979 <safetmdals79@gmail.com>
2025-07-29 20:32:32 +0900

    [Feat] SSM : 카메라 플레이어 따라가는 기능

--- Assets/Scripts/CharacterScripts/FollowCamera.cs
+++ Assets/Scripts/CharacterScripts/FollowCamera.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
+    public Vector3 offset;  // 카메라와 플레이어 사이 간격
+    public float smoothSpeed = 5f; // 부드럽게 따라오는 속도
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (target == null) return;
+
+        // 카메라 플레이어 따라가는 기능
+        Vector3 desiredPosition = new Vector3((target.position.x + offset.x), offset.y, offset.z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
     }
 }
