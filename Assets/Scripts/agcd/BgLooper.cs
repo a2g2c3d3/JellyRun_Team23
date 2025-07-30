cc43f66bcb7c6242b4f8938db51be41cfa5422ba
a2g2c3d3 <papa3520@naver.com>
2025-07-30 16:52:00 +0900

    feat : 패턴

--- Assets/Scripts/agcd/BgLooper.cs
+++ Assets/Scripts/agcd/BgLooper.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class BgLooper : MonoBehaviour
 {
     public int numBgCount = 4;
+    public int numPtCount = 1;
 
     // Start is called before the first frame update
 
     private void OnTriggerEnter2D(Collider2D collision) // 충돌에 대한 통보만 해주기때문에 부딪친 충돌체에대한 정보만 줌
     {
-        Debug.Log("Triggerd: " + collision.name); // 누구와 부딪쳤는지 확인
-
+       
         if (collision.CompareTag("Background"))
         {
             float widthOfBgObject = ((BoxCollider2D)collision).size.x;
             Vector3 pos = collision.transform.position;
 
-            pos.x += widthOfBgObject * numBgCount;
+            pos.x += widthOfBgObject * numBgCount; //백그라운드의 수만큼 띄어서 다시 위치시킴
             collision.transform.position = pos;
-            return; // 백그라운드라면 옵스타클이아닌걸 알기때문에
+            return; 
         }
 
-      
+        if (collision.CompareTag("Pattern"))
+        {
+            float widthOfBgObject = ((BoxCollider2D)collision).size.x;
+            Vector3 pos = collision.transform.position;
+
+            pos.x += widthOfBgObject * numPtCount;
+            collision.transform.position = pos;
+            return; 
+        }
+
     }
 
 }
---
+++ Assets/Scripts/agcd/Pattern.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Pattern : MonoBehaviour
+{
+    public Transform topObject;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
