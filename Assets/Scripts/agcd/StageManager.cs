79df694642c64a9bce4dcb4abf48e10802001e53
a2g2c3d3 <papa3520@naver.com>
2025-07-31 14:05:02 +0900

    fix : 스테이지변경

--- Assets/Scripts/agcd/StageManager.cs
+++ Assets/Scripts/agcd/StageManager.cs
@@ -1,21 +1,23 @@
+using System;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
     public GameObject[] stagePrefabs;       // Stage1, Stage2, ... 프리팹들
-    public float stageDuration = 30f;       // 스테이지 전환 시간
-    public PatternManager patternManager;
+    public float stageDuration = 10f;       // 스테이지 전환 시간
+    public float stageSpacing = 15f;
+    public Transform player;
+
 
     private int currentStageIndex = 0;
     private float timer;
+    private GameObject currentStageInstance;
 
-    private BgLooper bgLooper;
 
     void Start()
     {
-        bgLooper = FindObjectOfType<BgLooper>();
         timer = stageDuration;
-
+ 
         SpawnStage(currentStageIndex);
     }
 
@@ -25,12 +27,21 @@ public class StageManager : MonoBehaviour
 
         if (timer <= 0f)
         {
+            Debug.Log("스테이지 변경!");
+
+            // 이전 스테이지 제거
+            if (currentStageInstance != null)
+            {
+              
+                Destroy(currentStageInstance, 2f);
+            }
+
             currentStageIndex++;
             timer = stageDuration;
 
             if (currentStageIndex < stagePrefabs.Length)
             {
-                SpawnStage(currentStageIndex);
+                SpawnNextStage(currentStageIndex);
             }
             else
             {
@@ -41,6 +52,13 @@ public class StageManager : MonoBehaviour
 
     void SpawnStage(int index)
     {
-        Instantiate(stagePrefabs[index], Vector3.zero, Quaternion.identity);
+        currentStageInstance = Instantiate(stagePrefabs[index], Vector3.zero, Quaternion.identity);
+    }
+
+    void SpawnNextStage(int index)
+    {
+        float spawnX = player.position.x + stageSpacing;
+        Vector3 spawnPos = new Vector3(spawnX, 0f, 0f);
+        currentStageInstance = Instantiate(stagePrefabs[index], spawnPos , Quaternion.identity);
     }
 }
