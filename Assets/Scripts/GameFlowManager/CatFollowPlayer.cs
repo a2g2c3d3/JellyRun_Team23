1b71af7cc5c785f336d47f3c1a38038854c7a2e2
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-08-01 17:52:04 +0900

    [Feat] : 냥이 구현
    고양이 : 애니메이션,이동과 아이템 값을 저장하는 프립스 스크립트 두개 세개,

--- Assets/Scripts/GameFlowManager/CatFollowPlayer.cs
+++ Assets/Scripts/GameFlowManager/CatFollowPlayer.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class CatFollowPlayer : MonoBehaviour
 {
     public Transform target;
+    private void Awake()
+    {
+        target = GameObject.Find("Player").transform;
+    }
 
-    private void Update()
+    void FixedUpdate()
     {
         if(target != null)
         {
-            Vector3 followPos = target.position + new Vector3(-1.5f,0,0); //³»°Ô·Î ´Ù°¡¿Í »ìÂ¦Äô
+            Vector3 followPos = target.position + new Vector3(-0.5f,0,0); //³»°Ô·Î ´Ù°¡¿Í »ìÂ¦Äô
             transform.position = Vector3.Lerp(transform.position, followPos, Time.deltaTime * 5f);
+            
         }
     }
 }
--- Assets/Scripts/GameFlowManager/CatFollowerLoader.cs
+++ Assets/Scripts/GameFlowManager/CatFollowerLoader.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class CatFollowerLoader : MonoBehaviour
 {
     public Transform playertransform;
+    public List<GameObject> petPrefabs; // 펫 프리팹 리스트
 
     private void Start()
     {
-        string selectedCat = PlayerPrefs.GetString("SelectedCharacter","");
+
+        string selectedCat = PlayerPrefs.GetString("SelectedCharacter");
 
         if (!string.IsNullOrEmpty(selectedCat))
             {
-            GameObject catPrefab = Resources.Load<GameObject>($"Followers/{selectedCat}");
+            GameObject catPrefab = Resources.Load<GameObject>("Followers/" + selectedCat);
 
             if(catPrefab != null)
             {
                 GameObject cat = Instantiate(catPrefab, playertransform.position + new Vector3(1f,0,0), Quaternion.identity);
-                cat.AddComponent<CatFollowPlayer>().target = playertransform;
+                //cat.AddComponent<CatFollowPlayer>().target = playertransform;
+                Debug.Log("여기가 문제가 아니야 보석아");
+            }
+            else
+            {
+                Debug.Log("문제야 문제 이세상 속에");
             }
         }
+        else
+        {
+            Debug.Log("똑같은 사랑 노래가");
+        }
     }
+
 }
